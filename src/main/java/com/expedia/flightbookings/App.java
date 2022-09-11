efc9c756d9ce6ced659a0556fc3f43d127e87a65
srinuvas <p.srinu4b1@gmail.com>
2022-09-11 09:43:15 +0530

    udemy concepts testing

---
+++ src/main/java/com/expedia/flightbookings/App.java
@@ -0,0 +1,13 @@
+package com.expedia.flightbookings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
